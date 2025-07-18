3c295a4e700721247f6f41c32c749f8570be04eb
Vaibhav <bhagwatvr2004@gmail.com>
2025-07-18 21:00:21 +0530

    Restrict branch to documentation only

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Transpile ESM-only packages for both client and server
-  transpilePackages: ['lucide-react'],
-};
-
-module.exports = nextConfig;
